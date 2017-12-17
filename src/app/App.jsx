0dcbfe326d9224d66dfece6321be86f929cc951c
Shaneeth <shaneeth@live.in>
2017-12-17 23:07:26 +0530

    Added bootstrap and places, bikes, donuts module

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Nav from '@common/nav';
 import Dashboard from './dashboard';
 import Examples from './examples';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import './styles.less';
 import './styles.sass';
@@ -12,8 +13,10 @@ class App extends Component {
         return (
             <div>
                 <Nav />
-                <Route exact={true} path="/" component={Dashboard} />
-                <Route path="/:type" component={Examples} />
+                <div className="content">
+                    <Route exact={true} path="/" component={Dashboard} />
+                    <Route path="/:type" component={Examples} />
+                </div>                
             </div>        
         )
     }
--- src/app/dashboard/Dashboard.jsx
+++ src/app/dashboard/Dashboard.jsx
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
+import ButtonBase from '@mui/ButtonBase';
+
+import './styles';
 
 class Dashboard extends Component {
     render() {
-        return <h1>Dashboard...{this.props.test}</h1>;
+        return (
+            <div className="dashboard">
+                <div className="row">
+                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                        <ButtonBase className="stat c-orange">
+                            <div className="count">20</div>
+                            <div className="title">Total</div>
+                        </ButtonBase>
+                    </div>
+                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                        <ButtonBase className="stat c-red">
+                            <div className="count">20</div>
+                            <div className="title">Total</div>
+                        </ButtonBase>
+                    </div>
+                    <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                        <ButtonBase className="stat c-purple">
+                            <div className="count">20</div>
+                            <div className="title">Total</div>
+                        </ButtonBase>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 

804873113f5466444f17db38130c154fd4744082
Shaneeth <shaneeth@live.in>
2017-12-21 01:11:46 +0530

    Created server files for http and http2

--- server/helper.js
+++ server/helper.js
@@ -16,6 +16,7 @@ function getFiles (baseDir) {
     files.set(`/${fileName}`, {
       fileDescriptor,
       headers: {
+        'Cache-Control': 'max-age=20, public',
         'content-length': stat.size,
         'last-modified': stat.mtime.toUTCString(),
         'content-type': contentType
@@ -47,7 +48,46 @@ function getSpecificFile(filePath) {
     return null;    
 }
 
+function getDonutFiles(baseDir) {
+  const files = new Map()
+  const relativePath = '/assets/json/donuts';
+  const actualPath = path.join(baseDir, relativePath);
+  
+    fs.readdirSync(actualPath).forEach((fileName) => {
+      const filePath = path.join(actualPath, fileName)
+      const fileDescriptor = fs.openSync(filePath, 'r')
+      const stat = fs.fstatSync(fileDescriptor)
+      const contentType = mime.lookup(filePath)
+  
+      files.set(`${relativePath}/${fileName}`, {
+        fileDescriptor,
+        headers: {
+          'content-length': stat.size,
+          'last-modified': stat.mtime.toUTCString(),
+          'content-type': contentType
+        }
+      })
+    })
+    return files
+}
+
+function mapDonuts(url) {
+  const regEx = new RegExp('/assets/json/donuts/(.*).json', 'g')
+  const match = regEx.exec(url);
+  let donutInfo = {}, page;
+
+  if(match) {
+    page = Number(match[1]);
+    donutInfo.currentPath = `/assets/json/donuts/${page}.json`;
+    donutInfo.nextPath = `/assets/json/donuts/${page+1}.json`;
+    return donutInfo;
+  }
+  return null;
+}
+
 module.exports = {
   getFiles,
-  getSpecificFile
+  getSpecificFile,
+  getDonutFiles,
+  mapDonuts
 }
